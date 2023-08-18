b4ba29012044b2bd46931e536044db0226340ee5
killerprojecte <admin@fastmcmirror.org>
2023-08-18 14:07:30 +0800

    Fix publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,6 @@ publishing {
             groupId = "dev.rgbmc"
             artifactId = "SimpleInventory"
             version = this.version + "-" + id()
-            artifact(tasks.jar)
         }
     }
 }
